0f8650bdd9e6384207e7e5e81efdc4706dbc696c
Pratham Bhatia <83857889+prathambhatia@users.noreply.github.com>
2025-07-13 22:40:26 +0530

    Delete frontend/src/components/ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
-
-  if (!token) {
-    return <Navigate to="/signin" replace />;
-  }
-
-  return children;
-}
