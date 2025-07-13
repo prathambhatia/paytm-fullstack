1b7862e8d124fa775fe95d1f0621ab6afe04af02
Pratham Bhatia <83857889+prathambhatia@users.noreply.github.com>
2025-07-13 17:07:35 +0530

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -15,7 +15,7 @@ export const Signin = () => {
         e.preventDefault();
 
         try {
-            const res = await axios.post("http://localhost:3000/api/v1/user/signin", {
+            const res = await axios.post("/api/v1/user/signin", {
                 username,
                 password
             });
