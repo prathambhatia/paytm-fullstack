37fdccaf0a844ef557d0b4051d370b1f12c1de43
Pratham Bhatia <83857889+prathambhatia@users.noreply.github.com>
2025-07-13 17:07:15 +0530

    Update SendMoney.jsx

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -30,7 +30,7 @@ const SendMoney = () => {
     const token = localStorage.getItem("token");
     try {
       await axios.post(
-        "http://localhost:3000/api/v1/account/transfer",
+        "/api/v1/account/transfer",
         {
           to: userId,
           amount: Number(amount),
